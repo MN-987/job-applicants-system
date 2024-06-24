dbd28cd5cee612a668e9eb80ae20ac6228a55a1c
Mostafa Nasser <mostafanasserm98@gmail.com>
2024-06-24 04:42:38 +0400

    updated config service file

--- frontend/src/services/config.service.js
+++ frontend/src/services/config.service.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
 //import.meta.env.VITE_REACT_APP_backend_URL
+/* 
+note if yor enocounter error while testing with jest 
+you need to hardocde the url in the axios instance
+e.g replace baseURL: process.env.REACT_APP_BACKEND_URL with 
+baseURL: 'http://localhost:5000/api/'
+
+or process.env.REACT_APP_BACKEND_URL
+*/
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/'
+    baseURL: import.meta.env.VITE_REACT_APP_backend_URL
 });
